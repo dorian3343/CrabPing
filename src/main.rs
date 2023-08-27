f5c320bbfaf88b4f66a434fed5e0ef02f80d7ac5
Dorian Kalaczynski <dorian3343@gmail.com>
2023-08-28 00:00:01 +0200

    working on the timing

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,30 @@ async fn send_req(adr: &str, id:&u32) -> Result<(), Box<dyn std::error::Error + 
             return Ok(());
         }
     };
-    let start_time = Instant::now();
     let res = client.get(uri.clone()).await.unwrap();
-    let end_time = Instant::now();
-    let elapsed_time = end_time - start_time;
     let status = res.status().clone(); 
     let body_bytes = hyper::body::to_bytes(res.into_body()).await?;
     let body_str = String::from_utf8(body_bytes.to_vec())?;
+
     println!(
-        "Request Number [{}]\nRequest took: [{:.2?}]\nStatus: [{}]\nContents: n[\n{}\n]",
+        "Request Number [{}]\nStatus: [{}]\nContents: n[\n{}\n]",
         id,
-        elapsed_time,
         status,
         body_str
     );
 
     Ok(())
+}
+
+
+fn calculate_average_duration(durations: &Vec<Duration>) -> Duration {
+    let num_durations = durations.len() as u32;
+
+    if num_durations > 0 {
+        *durations.last().unwrap() / num_durations
+    } else {
+        Duration::from_secs(0)
+    }
 }
 
 
@@ -40,15 +48,19 @@ async fn main() {
             println!("#CrabPing [HttpReq] [Amount]\n============================");
             println!("#Running just CrabPing shows the help menu that you're using");
             println!("#[HttpReq] this is the endpoint you want to hit");
-            println!("#[Amount] how many requests you want to send");
+            println!("#[Amount] how many requests you want to send, max is 1000");
         },
         2 => {
+            let start = Instant::now();
             let _ = send_req(&args[1].to_string(),&0).await.unwrap();
+            let end = Instant::now();
+            println!("Request took: [{:.2?}]",end - start);
         },
         3 => {
             if let Ok(num) = args[2].parse::<u32>(){
                 let adr =  args[1].clone();
                 let mut handles = vec![];
+                let mut times: Vec<Duration> = vec![];
                 for i in 0..num{
                         let adr_clone = adr.clone();
                         let handle = tokio::spawn(async move {
@@ -57,8 +69,16 @@ async fn main() {
                         handles.push(handle);
                     }
                 for handle in handles{
-                        handle.await.expect("Failed to join thread"); 
+                        let start = Instant::now();
+                        handle.await.expect("Failed to join thread");
+                        let end = Instant::now();
+                        let dur = end - start;
+                        times.push(dur);
                     }
+                :x
+                println!("Speed Report:\n=============\nAverage:[{:.2?}]",
+                    calculate_average_duration(&times),
+                    );
                 }
                 else{
                 println!("Error: Paramater [{}] is non-numeric",args[2]);
