37bb601580e5de8603ab39d37b8e513771fd7811
Dorian Kalaczynski <dorian@dorian.home>
2023-08-30 00:48:14 +0100

    Refactored and made a good version

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
-use tokio::task;
 use std::env;
-use hyper::{Client, Uri};
-use std::time::{Instant, Duration};
-async fn send_req(adr: &str, id:&u32) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Client::new();
-    let uri = match adr.parse::<Uri>() {
-        Ok(uri) => uri,
-        Err(error) => {
-            println!("URI parsing error: {}", error);
-            return Ok(());
-        }
+use chrono::{Utc, DateTime, Duration};
+use hyper::{Client, Uri, body::to_bytes, http::Uri as HyperUri};
+use hyper_tls::HttpsConnector;
+use tokio::io::AsyncReadExt;
+use std::sync::mpsc;
+use tokio::runtime;
+
+#[derive(Debug)]
+struct ReqObj {
+    benchmark: i64,
+    id: u32,
+    status: String,
+    contents: String,
+}
+
+async fn send_req(adr: &str, id: &u32) -> Result<ReqObj, Box<dyn std::error::Error + Send + Sync>> {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let uri = adr.parse::<HyperUri>()?; // Use HyperUri instead of Uri
+    let start: DateTime<Utc> = Utc::now();
+    let res = client.get(uri.clone()).await?;
+    let end: DateTime<Utc> = Utc::now();
+    let dur: Duration = end - start;
+
+    let status = res.status().clone();
+    let body_bytes = to_bytes(res.into_body()).await?;
+    let body_str = String::from_utf8(body_bytes.to_vec())?; 
+
+   let obj = ReqObj {
+        benchmark: dur.num_milliseconds(),
+        id: *id,
+        status: status.to_string(),
+        contents: body_str,
     };
-    let res = client.get(uri.clone()).await.unwrap();
-    let status = res.status().clone(); 
-    let body_bytes = hyper::body::to_bytes(res.into_body()).await?;
-    let body_str = String::from_utf8(body_bytes.to_vec())?;
 
-    println!(
-        "Request Number [{}]\nStatus: [{}]\nContents: n[\n{}\n]",
-        id,
-        status,
-        body_str
-    );
-
-    Ok(())
+    Ok(obj)
 }
 
-
-fn calculate_average_duration(durations: &Vec<Duration>) -> Duration {
-    let num_durations = durations.len() as u32;
-
-    if num_durations > 0 {
-        *durations.last().unwrap() / num_durations
-    } else {
-        Duration::from_secs(0)
-    }
+fn calculate_average(vec: &Vec<i64>) -> f64 {
+    let sum: i64 = vec.iter().sum();
+    let count = vec.len() as f64;
+    let average = sum as f64 / count;
+    
+    average
 }
-
 
 #[tokio::main]
 async fn main() {
@@ -48,42 +55,56 @@ async fn main() {
             println!("#CrabPing [HttpReq] [Amount]\n============================");
             println!("#Running just CrabPing shows the help menu that you're using");
             println!("#[HttpReq] this is the endpoint you want to hit");
-            println!("#[Amount] how many requests you want to send, max is 1000");
+            println!("#[Amount] how many requests you want to send, max is 200");
         },
         2 => {
-            let start = Instant::now();
-            let _ = send_req(&args[1].to_string(),&0).await.unwrap();
-            let end = Instant::now();
-            println!("Request took: [{:.2?}]",end - start);
+            let request = send_req(&args[1].to_string(),&0).await.unwrap();
+            println!("Id: [{}]\nStatus: [{}]\nContents: [{}]\nBenchmark: [{} ms]",request.id,
+                     request.status,
+                     request.contents,
+                     request.benchmark,
+                     );
+
         },
-        3 => {
-            if let Ok(num) = args[2].parse::<u32>(){
-                let adr =  args[1].clone();
-                let mut handles = vec![];
-                let mut times: Vec<Duration> = vec![];
-                for i in 0..num{
-                        let adr_clone = adr.clone();
+        3 => { if args[2].parse::<u32>().is_ok(){
+                //todo
+                if args[2].parse::<u32>().unwrap() < 1 {
+                    println!("Error! Minimum requests is 1");
+                }else if args[2].parse::<u32>().unwrap() > 200 {
+                    println!("Error! Maximum requests is 200");
+                }else{
+                    let (tx,rx) = mpsc::channel();
+                    let mut handles = vec![];
+                    for i in 0.. args[2].parse::<u32>().unwrap(){
+                        let tx_clone = tx.clone();
+                        let adr = args[1].clone();
                         let handle = tokio::spawn(async move {
-                          let _ = send_req(&adr_clone.to_string(),&i).await.unwrap(); 
+                            let adr_clone = adr.clone();
+                            let request = send_req(&adr_clone,&i).await.unwrap();
+                            tx_clone.send(request).expect("Failed to send result");
                         });
                         handles.push(handle);
                     }
-                for handle in handles{
-                        let start = Instant::now();
-                        handle.await.expect("Failed to join thread");
-                        let end = Instant::now();
-                        let dur = end - start;
-                        times.push(dur);
+                    let mut times = vec![];
+                    for _ in handles {
+                        let catch = rx.recv().expect("Failed to send result");
+                     println!("Id: [{}]\nStatus: [{}]\nContents: [{}]\nBenchmark:[{} ms]",
+                              catch.id,
+                              catch.status,
+                              catch.contents,
+                              catch.benchmark);
+                     times.push(catch.benchmark);
                     }
-                :x
-                println!("Speed Report:\n=============\nAverage:[{:.2?}]",
-                    calculate_average_duration(&times),
-                    );
+                    let highest = *times.iter().max().unwrap();
+                    let smallest = *times.iter().min().unwrap();
+                    let avg = calculate_average(&times);
+                    println!("Benchmark Results:\n===============\nFastest:[{} ms]\nSlowest:[{} ms]\nAverage:[{} ms]"
+                             ,smallest,highest,avg);
                 }
-                else{
-                println!("Error: Paramater [{}] is non-numeric",args[2]);
+            }else{
+                println!("Error! Non numeric third paramater")
             }
-        },
+    },
         _ => {},
     };
 }
